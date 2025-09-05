c822035c470a40ffe93d3560b3e286b221bc7de9
AngeloGagno <angelogagno@gmail.com>
2025-09-05 17:44:45 -0300

    feat: docker

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Usando uma imagem oficial do Python
+FROM python:3.12
+
+# Definindo o diretório de trabalho dentro do container
+WORKDIR /app
+
+RUN pip install poetry
+# Copiando os arquivos necessários para a instalação do Poetry e dependências
+COPY pyproject.toml poetry.lock ./
+
+# Instalando o Poetry
+RUN poetry config virtualenvs.create false && poetry install --no-root 
+
+# Copiando o restante do código
+COPY . .
+
+# Comando para rodar a aplicação
+CMD ["poetry", "run", "python", "src/main.py"]
--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String,Column,DateTime,Boolean,Integer
+from sqlalchemy import String,Column,Date,Boolean,Integer
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
@@ -12,13 +12,13 @@ class DescriptionData(Base):
     price_cluster = Column(String)
     base_price = Column(String)
     minimum_price = Column(String)
-    last_booking_date = Column(DateTime)
-    furthest_checkin_date = Column(DateTime)
+    last_booking_date = Column(Date)
+    furthest_checkin_date = Column(Date)
 
 class BookingUpdate(Base):
     __tablename__ = 'beyond_booking_update'
     beyond_id = Column(String,primary_key=True,index=True)
-    scrap_date = Column(DateTime,primary_key=True,index=True)
+    scrap_date = Column(Date,primary_key=True,index=True)
     booked_7_days = Column(Integer)
     booked_14_days = Column(Integer)
     booked_30_days = Column(Integer)
@@ -31,14 +31,14 @@ class BasePriceHistorical(Base):
     __tablename__ = 'beyond_base_historical_data'
     transaction_id = Column(String(100), primary_key=True)
     beyond_id = Column(String(50),primary_key=True,index=True)
-    price_date_stamp = Column(DateTime)
+    price_date_stamp = Column(Date)
     value = Column(String(20))
-    scrap_date = Column(DateTime)
+    scrap_date = Column(Date)
 
 class BaseMinHistorical(Base):
     __tablename__ = 'beyond_minimum_historical_data'
     transaction_id = Column(String(100), primary_key=True)
     beyond_id = Column(String(50),primary_key=True,index=True)
-    price_date_stamp = Column(DateTime)
+    price_date_stamp = Column(Date)
     value = Column(String(20))
-    scrap_date = Column(DateTime)
+    scrap_date = Column(Date)
